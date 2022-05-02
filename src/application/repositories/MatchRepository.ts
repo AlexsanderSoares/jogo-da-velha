75d4fb460c6bcff217189182b7cdfc4b0edfac0d
AlexsanderSoares <alexsandersoares30@gmail.com>
2022-05-01 21:01:24 -0300

    Configurando typescript e consertando erros

--- src/application/repositories/MatchRepository.ts
+++ src/application/repositories/MatchRepository.ts
@@ -2,6 +2,7 @@ import { Match } from "../../domain/entities/Match";
 
 export interface MatchRepository{
     findById(id: string): Promise<Match | null>;
-    create(): Promise<null>;
-    update(): Promise<Match>;
+    findByRoomId(id: string): Promise<Match | null>;
+    create(match: Match): Promise<void>;
+    update(newMatch: Match): Promise<Match>;
 }
